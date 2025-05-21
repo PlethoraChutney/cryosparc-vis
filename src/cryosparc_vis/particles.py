28d99468601ecaf7c662fecd15b336ffb3a95e6a
Rich <rich@posert.com>
2025-05-21 08:42:20 -0700

    Initial particle plotting

--- src/cryosparc_vis/particles.py
+++ src/cryosparc_vis/particles.py
@@ -1,12 +1,16 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 from cryosparc.dataset import Dataset
 import numpy as np
+import matplotlib.pyplot as plt
 
 if TYPE_CHECKING:
+    import matplotlib.axes
+    import matplotlib.figure
     from .vis_dataset import VisDataset
     from numpy.typing import NDArray
     from collections.abc import Callable
     from cryosparc.row import Row
+    from cryosparc.column import Column
 
 class Particles:
     def __init__(self, parent:"VisDataset") -> None:
@@ -38,7 +42,7 @@ class Particles:
     
 
     @property
-    def particles(self) -> "None|Dataset":
+    def particles(self) -> "Dataset":
         bool_mask = np.all(list(self.bool_masks.values()), axis = 0)
         p = self.unmasked_particles.mask(bool_mask)
         for f_mask in self.function_masks.values():
@@ -57,3 +61,73 @@ class Particles:
             self.remove_mask("mic_uid")
         else:
             self.bool_masks["mic_uid"] = self.unmasked_particles["location/micrograph_uid"] == self.parent.mic_uid
+
+    @property
+    def fields(self) -> list[str]:
+        return self.particles.fields()
+    
+    def fields_prefix(self, prefix:str) -> list[str]:
+        return list(f for f in self.fields if f.startswith(prefix))
+
+    # plotting
+
+    @property
+    def particles_x(self) -> "Column":
+        return self.particles["location/center_x_frac"]
+    
+    @property
+    def particles_y(self) -> "Column":
+        return self.particles["location/center_y_frac"]
+    
+    plot_defaults = {
+        "s": 40,
+        "facecolor": "#FAB06E",
+        "edgecolor": "black",
+        "linewidth": 1
+    }
+
+    def plot(
+        self,
+        ax:Optional["matplotlib.axes.Axes"] = None,
+        figsize:Optional[tuple[float, float]] = None,
+        color_by:Optional[str] = None,
+        legend:Optional[bool] = True,
+        **kwargs,
+    ) -> tuple["matplotlib.figure.Figure", "matplotlib.axes.Axes"]:
+        if ax is None:
+            fig, ax = plt.subplots(
+                1,
+                1,
+                frameon = False,
+                layout = "constrained",
+                figsize = figsize if figsize else self.parent.figsize
+            )
+        else:
+            fig = ax.get_figure()
+
+        defaults = self.__class__.plot_defaults.copy()
+        defaults.update(**kwargs)
+
+        if color_by is not None:
+            del defaults["facecolor"]
+
+        scatter = ax.scatter(
+            self.particles_x,
+            self.particles_y,
+            c = self.particles[color_by] if color_by is not None else None,
+            **defaults
+        )
+        if color_by is not None and legend:
+            ax.add_artist(
+                ax.legend(*scatter.legend_elements(), loc = "upper right", title = color_by)
+            )
+        ax.set_aspect("equal")
+        ax.axis("off")
+        ax.margins(0)
+
+        
+        
+        return fig, ax # type: ignore
+        
+
+
